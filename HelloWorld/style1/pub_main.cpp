bea41a1fcc845b8a35142f8ba28060b86f50e123
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-07 13:46:42 +0900

    1st time calling fastrtps object, modify Makefile to acount for this change

--- HelloWorld/style1/pub_main.cpp
+++ HelloWorld/style1/pub_main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fastrtps/participant/Participant.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+	eprosima::fastrtps::Participant* pParticipant;
 	cout<<"end of pub"<<endl;
 	return 0;
 }
