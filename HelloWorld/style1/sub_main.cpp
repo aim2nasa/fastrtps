74347b3227ee93c5731942abbb44dc7a86753a44
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 15:24:00 +0900

    create participant in subscriber

--- HelloWorld/style1/sub_main.cpp
+++ HelloWorld/style1/sub_main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <fastrtps/participant/Participant.h>
+#include <fastrtps/Domain.h>
+#include <fastrtps/attributes/ParticipantAttributes.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
+	eprosima::fastrtps::ParticipantAttributes PParam;
+	eprosima::fastrtps::Participant* pParticipant = eprosima::fastrtps::Domain::createParticipant(PParam);
+	assert(pParticipant!=NULL);
+
 	cout<<"end of sub"<<endl;
 	return 0;
 }
