4b1c0c530feb372c1ee58fb758646e921a3796bc
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 13:38:22 +0900

    link HelloWorldPubSubTypes from pub_main and move compiled type library to parent folder

--- HelloWorld/style1/pub_main.cpp
+++ HelloWorld/style1/pub_main.cpp
@@ -2,6 +2,7 @@
 #include <fastrtps/participant/Participant.h>
 #include <fastrtps/Domain.h>
 #include <fastrtps/attributes/ParticipantAttributes.h>
+#include "gen/HelloWorldPubSubTypes.h"
 
 using namespace std;
 
@@ -10,6 +11,9 @@ int main(int argc, char** argv)
 	eprosima::fastrtps::ParticipantAttributes PParam;
 	eprosima::fastrtps::Participant* pParticipant = eprosima::fastrtps::Domain::createParticipant(PParam);
 	assert(pParticipant!=NULL);
+
+	HelloWorldPubSubType type;
+
 	cout<<"end of pub"<<endl;
 	return 0;
 }
