17dc34b0c9c91f8c3f57f41fa71e2e975cdf3914
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 15:50:32 +0900

    remove participant call

--- HelloWorld/style1/sub_main.cpp
+++ HelloWorld/style1/sub_main.cpp
@@ -11,6 +11,10 @@ int main(int argc, char** argv)
 	eprosima::fastrtps::Participant* pParticipant = eprosima::fastrtps::Domain::createParticipant(PParam);
 	assert(pParticipant!=NULL);
 
+	bool b;
+	b = eprosima::fastrtps::Domain::removeParticipant(pParticipant);
+	assert(b==true);
+
 	cout<<"end of sub"<<endl;
 	return 0;
 }
