624c433d46aa6cd4a14c6a9aa24c1c85243a86c0
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 14:12:55 +0900

    create publisher

--- HelloWorld/style1/pub_main.cpp
+++ HelloWorld/style1/pub_main.cpp
@@ -2,6 +2,7 @@
 #include <fastrtps/participant/Participant.h>
 #include <fastrtps/Domain.h>
 #include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/PublisherAttributes.h>
 #include "gen/HelloWorldPubSubTypes.h"
 
 using namespace std;
@@ -13,6 +14,14 @@ int main(int argc, char** argv)
 	assert(pParticipant!=NULL);
 
 	HelloWorldPubSubType type;
+	eprosima::fastrtps::Domain::registerType(pParticipant,&type);
+
+	eprosima::fastrtps::PublisherAttributes WParam;
+	WParam.topic.topicDataType = "HelloWorld";
+
+	eprosima::fastrtps::Publisher* pPublisher 
+		= eprosima::fastrtps::Domain::createPublisher(pParticipant,WParam);
+	assert(pPublisher!=NULL);
 
 	cout<<"end of pub"<<endl;
 	return 0;
