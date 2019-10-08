056de671d5cfe655daaec099b18301a22e059173
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 16:27:37 +0900

    waiting mark display

--- HelloWorld/style1/sub_main.cpp
+++ HelloWorld/style1/sub_main.cpp
@@ -44,7 +44,9 @@ int main(int argc, char** argv)
 	assert(pSubscriber!=NULL);
 
 	while(ls.nCount_==0) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::cout<<".";
+		std::fflush(stdout);
 	}
 
 	bool b;
