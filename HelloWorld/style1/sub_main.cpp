54490737b38c98e0ecfa790d1fcac379fb796ec5
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-08 15:58:17 +0900

    create subscriber

--- HelloWorld/style1/sub_main.cpp
+++ HelloWorld/style1/sub_main.cpp
@@ -2,6 +2,7 @@
 #include <fastrtps/participant/Participant.h>
 #include <fastrtps/Domain.h>
 #include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/SubscriberAttributes.h>
 #include "gen/HelloWorldPubSubTypes.h"
 
 using namespace std;
@@ -15,7 +16,16 @@ int main(int argc, char** argv)
 	HelloWorldPubSubType type;
 	eprosima::fastrtps::Domain::registerType(pParticipant,&type);
 
+	eprosima::fastrtps::SubscriberAttributes RParam;
+	RParam.topic.topicDataType = "HelloWorld";
+
+	eprosima::fastrtps::Subscriber* pSubscriber
+		= eprosima::fastrtps::Domain::createSubscriber(pParticipant,RParam);
+	assert(pSubscriber!=NULL);
+
 	bool b;
+	b = eprosima::fastrtps::Domain::removeSubscriber(pSubscriber);
+	assert(b==true);
 	b = eprosima::fastrtps::Domain::removeParticipant(pParticipant);
 	assert(b==true);
 
