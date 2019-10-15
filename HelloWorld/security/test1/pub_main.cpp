ff42172e8fc3a877cb4e3f7a225d02f140e54264
Rossi Kwak <rossi.kwak@gmail.com>
2019-10-15 15:13:42 +0900

    reference pub/sub example copied from 'compExp/style1' which has no security

---
+++ HelloWorld/security/test1/pub_main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fastrtps/participant/Participant.h>
+#include <fastrtps/Domain.h>
+#include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/PublisherAttributes.h>
+#include <fastrtps/publisher/Publisher.h>
+#include "gen/HelloWorldPubSubTypes.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	eprosima::fastrtps::ParticipantAttributes PParam;
+	eprosima::fastrtps::Participant* pParticipant = eprosima::fastrtps::Domain::createParticipant(PParam);
+	assert(pParticipant!=NULL);
+
+	HelloWorldPubSubType type;
+	eprosima::fastrtps::Domain::registerType(pParticipant,&type);
+
+	eprosima::fastrtps::PublisherAttributes WParam;
+	WParam.topic.topicDataType = "HelloWorld";
+
+	eprosima::fastrtps::Publisher* pPublisher 
+		= eprosima::fastrtps::Domain::createPublisher(pParticipant,WParam);
+	assert(pPublisher!=NULL);
+
+	bool b;
+
+	HelloWorld hello;
+	hello.msg("HelloWorld");
+	for(int i=0;i<10;i++)
+	{
+		b = pPublisher->write((void*)&hello);
+		if(b) {
+			std::cout<<".";
+			std::fflush(stdout);
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+
+	b = eprosima::fastrtps::Domain::removePublisher(pPublisher);
+	assert(b==true);
+	b = eprosima::fastrtps::Domain::removeParticipant(pParticipant);
+	assert(b==true);
+
+	cout<<"end of pub"<<endl;
+	return 0;
+}
---
+++ HelloWorld/security/test1/sub_main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fastrtps/participant/Participant.h>
+#include <fastrtps/Domain.h>
+#include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/SubscriberAttributes.h>
+#include <fastrtps/subscriber/SubscriberListener.h>
+#include <fastrtps/subscriber/SampleInfo.h>
+#include <fastrtps/subscriber/Subscriber.h>
+#include "gen/HelloWorldPubSubTypes.h"
+
+using namespace std;
+
+class listener:public eprosima::fastrtps::SubscriberListener{
+public:
+	listener():nCount_(0){}
+	~listener(){}
+
+	int nCount_;
+	eprosima::fastrtps::SampleInfo_t info_;
+
+	void onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub, eprosima::fastrtps::rtps::MatchingInfo& info);
+	void onNewDataMessage(eprosima::fastrtps::Subscriber* sub);
+};
+void listener::onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub, eprosima::fastrtps::rtps::MatchingInfo& info)
+{
+	(void)sub;
+
+	if(info.status==eprosima::fastrtps::rtps::MATCHED_MATCHING)
+	{
+		std::cout<<"Subscriber matched"<<std::endl;
+	}
+	else
+	{
+		std::cout<<"Subscriber unmatched"<<std::endl;
+	}
+}
+
+void listener::onNewDataMessage(eprosima::fastrtps::Subscriber* sub)
+{
+	HelloWorld st;
+	if(sub->takeNextData(&st,&info_))
+	{
+		if(info_.sampleKind == eprosima::fastrtps::rtps::ALIVE)
+		{
+			std::cout<<"Message="<<st.msg()<<std::endl;
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	eprosima::fastrtps::ParticipantAttributes PParam;
+	eprosima::fastrtps::Participant* pParticipant = eprosima::fastrtps::Domain::createParticipant(PParam);
+	assert(pParticipant!=NULL);
+
+	HelloWorldPubSubType type;
+	eprosima::fastrtps::Domain::registerType(pParticipant,&type);
+
+	eprosima::fastrtps::SubscriberAttributes RParam;
+	RParam.topic.topicDataType = "HelloWorld";
+
+	listener ls;
+	eprosima::fastrtps::Subscriber* pSubscriber
+		= eprosima::fastrtps::Domain::createSubscriber(pParticipant,RParam,(eprosima::fastrtps::SubscriberListener*)&ls);
+	assert(pSubscriber!=NULL);
+
+	while(ls.nCount_==0) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		std::cout<<".";
+		std::fflush(stdout);
+	}
+
+	bool b;
+	b = eprosima::fastrtps::Domain::removeSubscriber(pSubscriber);
+	assert(b==true);
+	b = eprosima::fastrtps::Domain::removeParticipant(pParticipant);
+	assert(b==true);
+
+	cout<<"end of sub"<<endl;
+	return 0;
+}
